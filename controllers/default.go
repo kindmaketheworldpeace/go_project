21c8ded3d305453a1fe51e94b6c093a23b009617
renan <3383109098@qq.com>
2020-04-24 13:08:32 +0800

    feat:add frontend file

--- controllers/default.go
+++ controllers/default.go
@@ -1,13 +1,27 @@
 package controllers
 
 import (
+	"fmt"
+	"myproject/models"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 )
 
 type MainController struct {
 	beego.Controller
 }
+type UserController struct {
+	beego.Controller
+}
 
 func (c *MainController) Get() {
 	c.TplName = "index.html"
+}
+func (c *UserController) Get() {
+	o := orm.NewOrm()
+	user := models.User{Id: 1}
+	err := o.Read(&user)
+	c.Data["json"] = user
+	c.ServeJSON()
 }
--- models/models.go
+++ models/models.go
@@ -1,4 +1,4 @@
-
+package models
 
 import (
 	"github.com/astaxie/beego/orm"
